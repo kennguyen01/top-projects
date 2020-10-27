84e2ff03fbd1b6d68e6e81c3911e766aef5bb8bf
Ken Nguyen <nguyen.ken.v@gmail.com>
2020-10-26 20:29:02 -0400

    Create modal to add new book

--- library/script.js
+++ library/script.js
@@ -1,5 +1,5 @@
 /**
- * Create a new book.
+ * Create a new book object.
  * 
  * @constructor
  * @param {string} title 
@@ -7,69 +7,99 @@
  * @param {string} genre
  * @param {boolean} finished 
  */
-function Book(title, author, genre, finished) {
+function Book(title, author, genre, status) {
   this.title = title;
   this.author = author;
   this.genre = genre;
-  this.finished = finished;
+  this.status = status;
 }
 
 /**
- * Add book to library.
+ * Verify if user inputs valid book data.
+ * Display warnings if it's not.
  * 
- * @param {Object} book 
- * @param {Array} library 
+ * @param {(number|Array.<String>)} book - 0 or array of strings
+ * @param {HTMLElement} mod - Add book modal
  */
-const addToLibrary = (book, library) => {
-
+const verifyData = (data, mod) => {
+  // Missing input field
+  if (data.includes('')) {
+    let warn = document.querySelector('.modal-warning');
+    warn.textContent = 'Missing book information';
+  } else {
+    mod.style.display = 'none';
+    return data
+  }
 }
 
 /**
- * Display each book in the library.
+ * Get data from modal input.
+ * Return 0 if an input field is missing.
  * 
- * @param {Array} library 
+ * @returns {(number|Array.<String>)} 0 or array of strings
  */
-const showBooks = library => {
+const getBookData = (mod) => {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let genre = document.querySelector('#genre').value;
+  let status = document.querySelector('#status').value;
 
+  let data = [title, author, genre, status];
+  return verifyData(data, mod);
 }
 
+
 /**
- * Open and close add book modal.
+ * Open and close add book modal with listeners.
  * 
- * @param {HTMLElement} btn 
+ * @param {HTMLElement} mod - Add book modal
  */
-const addBook = () => {
-  let modal = document.querySelector('.modal');
+const modalControl = (mod) => {
+
+  function closeMod() {
+    mod.style.display = 'none';
+  }
 
   // Open modal when 'Add Book' button clicked
-  let btn = document.querySelector('.add-modal');
+  let btn = document.querySelector('.open-modal');
   btn.addEventListener('click', () => {
-    modal.style.display = 'block';
+    mod.style.display = 'block';
   });
 
   // Close modal when X clicked
-  let close = document.querySelector('.close-modal')
-  close.addEventListener('click', () => {
-    modal.style.display = 'none';
+  let closeBtn = document.querySelector('.close-modal')
+  closeBtn.addEventListener('click', () => {
+    closeMod();
   });
 
   // Close modal when user click outside the box
-  window.addEventListener('click', e => {
-    if (e.target == modal) {
-      modal.style.display = 'none';
+  window.addEventListener('click', (e) => {
+    if (e.target == mod) {
+      closeMod();
     }
   });
 
   // Close modal when 'Cancel' button clicked
   let cancel = document.querySelector('.add-cancel')
   cancel.addEventListener('click', () => {
-    modal.style.display = 'none';
+    closeMod();
   });
 }
 
 (function() {
   let userLibrary = [];
+  let book;
 
-  // Add book modal
-  addBook();
+  // Activate modal
+  let modal = document.querySelector('.modal');
+  modalControl(modal);
+
+  // Verify and add new book to library
+  let addBtn = document.querySelector('.add-book');
+  addBtn.addEventListener('click', () => {
+    book = getBookData(modal);
+
+    // Create new book
+    book = new Book(...book);
+  });
 })();
